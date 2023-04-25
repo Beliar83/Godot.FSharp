1f0314dffbea37bbddba426ea1ec4fc40a92ff3b
Karsten Bock <KarstenBock@gmx.net>
2023-04-25 07:29:49 +0200

    Add GodotGenerator and helper functions for TAST

---
+++ Godot.FSharp/Godot.FSharp.SourceGenerators/GodotGenerator.fs
@@ -0,0 +1,30 @@
+﻿module Godot.FSharp.SourceGenerators.GodotGenerator
+
+open System
+open Myriad.Core
+
+[<MyriadGenerator("Godot.FSharp")>]
+type GodotGenerator() =
+    interface IMyriadGenerator with
+        member this.Generate(context) =
+            let generators: array<IGodotGenerator> = [||]
+
+            let generators =
+                generators
+                |> Array.map (fun x -> (x.GetNumberOfGeneratedTypes context, x))
+                |> Array.filter (fun (i, _) -> i > 0)
+
+            if generators.Length = 0 then
+                Output.Ast []
+            else
+                if generators.Length > 1
+                   || generators |> Array.exists (fun (i, _) -> i > 1) then
+                    $"{context.InputFilename} should only contain one godot type to generate"
+                    |> Exception
+                    |> raise
+
+                let _, generator = generators |> Array.head
+
+                generator.Generate context
+
+        member this.ValidInputExtensions = seq { ".fs" }
