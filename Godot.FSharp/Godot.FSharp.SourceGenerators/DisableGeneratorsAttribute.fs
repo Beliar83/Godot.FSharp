baea53edd486e5fc526c37358ed50674ff9da685
Karsten Bock <KarstenBock@gmx.net>
2023-06-19 14:29:25 +0200

    Add DisableGeneratorsAttribute

---
+++ Godot.FSharp/Godot.FSharp.SourceGenerators/DisableGeneratorsAttribute.fs
@@ -0,0 +1,8 @@
+﻿namespace Godot
+
+open System
+
+[<AttributeUsage(validOn = AttributeTargets.Class, Inherited = false)>]
+type DisableGeneratorsAttribute() =
+    inherit Attribute()
+    new (generators: string[]) = DisableGeneratorsAttribute()
